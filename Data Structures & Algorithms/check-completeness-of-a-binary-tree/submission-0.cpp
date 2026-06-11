ffb23118501c5f27af42e1f74a0aab1f62650318
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2026-06-11 18:03:33 +0530

    Add: check-completeness-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/check-completeness-of-a-binary-tree/submission-0.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode dummy(69);
+
+        while(!q.empty()) {
+            int size = q.size();
+            TreeNode* prev = &dummy;
+
+            for(int i = 0; i<size; i++) {
+                TreeNode* curr = q.front(); q.pop();
+                if(!prev && curr) return false; // If prev is null but curr is defined
+
+                if(curr) {
+                    q.push(curr->left);
+                    q.push(curr->right);
+                }
+                prev = curr;
+            }
+        }
+        return true;
+    }
+};
